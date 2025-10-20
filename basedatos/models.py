81da6afe124685961ee803fb662adc14494de84e
natalia <nataliamelendez2426@gmail.com>
2025-10-20 17:52:14 -0500

    Auto-sync: file changes

--- basedatos/models.py
+++ basedatos/models.py
@@ -1,13 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from datetime import datetime
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, CheckConstraint, func
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-from datetime import date
-from flask_sqlalchemy import SQLAlchemy
-
-Base = declarative_base()
+from sqlalchemy import CheckConstraint, Enum
+from datetime import datetime, date
 
 db = SQLAlchemy()
 
@@ -44,6 +38,7 @@ class Usuario(UserMixin, db.Model):
     def __repr__(self):
         return f'<Usuario {self.Nombre} {self.Apellido or ""}>'
 
+
 # ------------------ Direccion ------------------
 class Direccion(db.Model):
     __tablename__ = 'Direccion'
@@ -57,6 +52,7 @@ class Direccion(db.Model):
     InfoAdicional = db.Column(db.String(200))
     Barrio = db.Column(db.String(100))
     Destinatario = db.Column(db.String(100))
+
 
 # ------------------ Proveedor ------------------
 class Proveedor(db.Model):
@@ -77,6 +73,7 @@ class Proveedor(db.Model):
     def __repr__(self):
         return f'<Proveedor {self.NombreEmpresa} - {self.Ciudad}>'
 
+
 # ------------------ Categorias ------------------
 class Categorias(db.Model):
     __tablename__ = 'Categorias'
@@ -86,6 +83,7 @@ class Categorias(db.Model):
     Descripcion = db.Column(db.Text)
 
     productos = db.relationship('Producto', back_populates='categoria', lazy=True)
+
 
 # ------------------ Producto ------------------
 class Producto(db.Model):
@@ -98,18 +96,19 @@ class Producto(db.Model):
     PrecioUnidad = db.Column(db.Float, nullable=False)
     Color = db.Column(db.String(30))
     Descripcion = db.Column(db.Text)
-    ImagenPrincipal = db.Column(db.String(200))  
+    ImagenPrincipal = db.Column(db.String(200))
     ID_Proveedor = db.Column(db.Integer, db.ForeignKey('Proveedor.ID_Proveedor'), nullable=False)
     ID_Categoria = db.Column(db.Integer, db.ForeignKey('Categorias.ID_Categoria'))
 
-    Comentario = db.Column(db.Text)  # Nuevo campo
-    Calificacion = db.Column(db.Integer)  # Nuevo campo
+    # Campos opcionales
+    Comentario = db.Column(db.Text)
+    Calificacion = db.Column(db.Integer)
 
     proveedor = db.relationship('Proveedor', back_populates='productos')
     categoria = db.relationship('Categorias', back_populates='productos')
     imagenes = db.relationship('ImagenProducto', backref='producto', lazy=True)
     novedades = db.relationship('Novedades', backref='producto', lazy=True)
-    detalles_pedido = db.relationship('Detalle_Pedido', backref='producto', lazy=True)
+    detalles_pedido = db.relationship('Detalle_Pedido', back_populates='producto', lazy=True)
     resenas = db.relationship('Resena', back_populates='producto', lazy=True)
 
 
@@ -121,18 +120,90 @@ class ImagenProducto(db.Model):
     ruta = db.Column(db.String(200), nullable=False)
     ID_Producto = db.Column(db.Integer, db.ForeignKey('Producto.ID_Producto'), nullable=False)
 
+
+# ------------------ Pedido ------------------
+class Pedido(db.Model):
+    __tablename__ = 'Pedido'
+
+    ID_Pedido = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    NombreComprador = db.Column(db.String(100))
+    Estado = db.Column(Enum('pendiente', 'en proceso', 'en reparto', 'entregado', name='estado_pedido'))
+    FechaPedido = db.Column(db.Date, default=date.today)
+    FechaEntrega = db.Column(db.Date)
+    Destino = db.Column(db.String(200))
+    Descuento = db.Column(db.Float, default=0)
+    ID_Usuario = db.Column(db.Integer, db.ForeignKey('Usuario.ID_Usuario'), nullable=False)
+    ID_Empleado = db.Column(db.Integer, db.ForeignKey('Usuario.ID_Usuario'))
+    HoraLlegada = db.Column(db.DateTime)
+
+    pagos = db.relationship('Pagos', backref='pedido', lazy=True)
+    detalles_pedido = db.relationship('Detalle_Pedido', backref='pedido', lazy=True)
+    firmas = db.relationship('Firmas', backref='pedido', lazy=True)
+    comentarios = db.relationship('Comentarios', backref='pedido', lazy=True)
+    calendario = db.relationship('Calendario', backref='pedido', lazy=True)
+
+
+# ------------------ Detalle Pedido ------------------
+class Detalle_Pedido(db.Model):
+    __tablename__ = 'Detalle_Pedido'
+
+    ID_Pedido = db.Column(db.Integer, db.ForeignKey('Pedido.ID_Pedido'), primary_key=True)
+    ID_Producto = db.Column(db.Integer, db.ForeignKey('Producto.ID_Producto'), primary_key=True)
+    Cantidad = db.Column(db.Integer, nullable=False)
+    PrecioUnidad = db.Column(db.Float, nullable=False)
+
+    producto = db.relationship('Producto', back_populates='detalles_pedido')
+
+    @property
+    def subtotal(self):
+        return self.Cantidad * self.PrecioUnidad
+
+
+# ------------------ Pagos ------------------
+class Pagos(db.Model):
+    __tablename__ = 'Pagos'
+
+    ID_Pagos = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    MetodoPago = db.Column(db.String(50))
+    FechaPago = db.Column(db.Date, default=date.today)
+    Monto = db.Column(db.Float)
+    ID_Pedido = db.Column(db.Integer, db.ForeignKey('Pedido.ID_Pedido'), nullable=False)
+
+
+# ------------------ Firmas ------------------
+class Firmas(db.Model):
+    __tablename__ = 'Firmas'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pedido_id = db.Column(db.Integer, db.ForeignKey('Pedido.ID_Pedido'), nullable=False)
+    nombre_cliente = db.Column(db.String(100), nullable=False)
+    firma = db.Column(db.Text, nullable=False)
+    fecha_firma = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+
+# ------------------ Comentarios ------------------
+class Comentarios(db.Model):
+    __tablename__ = 'Comentarios'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pedido_id = db.Column(db.Integer, db.ForeignKey('Pedido.ID_Pedido', ondelete="CASCADE"), nullable=False)
+    texto = db.Column(db.Text, nullable=False)
+    fecha = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+
 # ------------------ Calendario ------------------
 class Calendario(db.Model):
     __tablename__ = 'Calendario'
 
-    ID_Calendario = db.Column(db.Integer, primary_key=True)
+    ID_Calendario = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Fecha = db.Column(db.Date, nullable=False)
     Hora = db.Column(db.Time, nullable=False)
     Ubicacion = db.Column(db.String(255), nullable=False)
     Tipo = db.Column(db.String(50), nullable=False)
 
     ID_Usuario = db.Column(db.Integer, db.ForeignKey('Usuario.ID_Usuario'), nullable=False)
-    ID_Pedido = db.Column(db.Integer, db.ForeignKey('Pedido.ID_Pedido'), nullable=True)
+    ID_Pedido = db.Column(db.Integer, db.ForeignKey('Pedido.ID_Pedido'))
+
 
 # ------------------ Notificaciones ------------------
 class Notificaciones(db.Model):
@@ -145,6 +216,7 @@ class Notificaciones(db.Model):
     Leida = db.Column(db.Boolean, default=False)
     ID_Usuario = db.Column(db.Integer, db.ForeignKey('Usuario.ID_Usuario'), nullable=False)
 
+
 # ------------------ Novedades ------------------
 class Novedades(db.Model):
     __tablename__ = 'Novedades'
@@ -152,85 +224,14 @@ class Novedades(db.Model):
     ID_Novedad = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Tipo = db.Column(db.String(50))
     EstadoNovedad = db.Column(db.String(50))
-    FechaReporte = db.Column(db.Date)
-
+    FechaReporte = db.Column(db.Date, default=date.today)
     ID_Usuario = db.Column(db.Integer, db.ForeignKey('Usuario.ID_Usuario'), nullable=False)
     ID_Producto = db.Column(db.Integer, db.ForeignKey('Producto.ID_Producto'), nullable=False)
 
-# ------------------ Pedido ------------------
-from datetime import datetime
-
-class Pedido(db.Model):
-    __tablename__ = 'Pedido'
-
-    ID_Pedido = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    NombreComprador = db.Column(db.String(100))
-    Estado = db.Column(db.Enum('pendiente', 'en proceso', 'en reparto', 'entregado'))
-    FechaPedido = db.Column(db.Date)
-    FechaEntrega = db.Column(db.Date)
-    Destino = db.Column(db.String(200))
-    Descuento = db.Column(db.Float)
-    ID_Usuario = db.Column(db.Integer, db.ForeignKey('Usuario.ID_Usuario'), nullable=False)
-
-   
-    ID_Empleado = db.Column(db.Integer, db.ForeignKey('Usuario.ID_Usuario'))
-
-    HoraLlegada = db.Column(db.DateTime)
-
-    pagos = db.relationship('Pagos', backref='pedido', lazy=True)
-    detalles_pedido = db.relationship('Detalle_Pedido', back_populates='producto', lazy=True)
-    firmas = db.relationship('Firmas', backref='pedido', lazy=True)
-    comentarios = db.relationship('Comentarios', backref='pedido', lazy=True)
-    calendario = db.relationship('Calendario', backref='pedido', lazy=True)
-
-# ------------------ Pagos ------------------
-class Pagos(db.Model):
-    __tablename__ = 'Pagos'
-
-    ID_Pagos = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    MetodoPago = db.Column(db.String(50))
-    FechaPago = db.Column(db.Date)
-    Monto = db.Column(db.Float)
-    ID_Pedido = db.Column(db.Integer, db.ForeignKey('Pedido.ID_Pedido'), nullable=False)
-
-# ------------------ Detalle Pedido ------------------
-class Detalle_Pedido(db.Model):
-    __tablename__ = 'Detalle_Pedido'
-
-    ID_Pedido = db.Column(db.Integer, db.ForeignKey('Pedido.ID_Pedido'), primary_key=True)
-    ID_Producto = db.Column(db.Integer, db.ForeignKey('Producto.ID_Producto'), primary_key=True)
-    Cantidad = db.Column(db.Integer)
-    PrecioUnidad = db.Column(db.Float)
-
-   
-    producto = db.relationship('Producto', back_populates='detalles_pedido')
-
-    @property
-    def subtotal(self):
-        return self.Cantidad * self.PrecioUnidad
-# ------------------ Firmas ------------------
-class Firmas(db.Model):
-    __tablename__ = 'Firmas'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    pedido_id = db.Column(db.Integer, db.ForeignKey('Pedido.ID_Pedido'), nullable=False)
-    nombre_cliente = db.Column(db.String(100), nullable=False)
-    firma = db.Column(db.Text, nullable=False)
-    fecha_firma = db.Column(db.DateTime, default=db.func.current_timestamp())
-
-# ------------------ Comentarios ------------------
-class Comentarios(db.Model):
-    __tablename__ = 'Comentarios'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    pedido_id = db.Column(db.Integer, db.ForeignKey('Pedido.ID_Pedido', ondelete="CASCADE"), nullable=False)
-    texto = db.Column(db.Text, nullable=False)
-    fecha = db.Column(db.DateTime, default=db.func.current_timestamp())
 
 # ------------------ Reseñas ------------------
-
 class Resena(db.Model):
-    __tablename__ = 'Resena'  
+    __tablename__ = 'Resena'
 
     ID_Resena = db.Column(db.Integer, primary_key=True, autoincrement=True)
     ID_Producto = db.Column(db.Integer, db.ForeignKey('Producto.ID_Producto'), nullable=False)
@@ -239,22 +240,22 @@ class Resena(db.Model):
     Comentario = db.Column(db.Text, nullable=False)
     Fecha = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
 
-    producto = db.relationship("Producto", back_populates="resenas")
-    usuario = db.relationship("Usuario", back_populates="resenas")
+    producto = db.relationship('Producto', back_populates='resenas')
+    usuario = db.relationship('Usuario', back_populates='resenas')
 
     __table_args__ = (
         CheckConstraint('Calificacion >= 1 AND Calificacion <= 5', name='check_calificacion_range'),
     )
 
-# ------------------ Compras ------------------
 
+# ------------------ Compras ------------------
 class Compra(db.Model):
     __tablename__ = 'Compra'
 
     ID_Compra = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Producto = db.Column(db.String(100), nullable=False)
     Cantidad = db.Column(db.Integer, nullable=False)
-    Fecha = db.Column(db.Date, nullable=False, default=date.today)
+    Fecha = db.Column(db.Date, default=date.today)
     ProveedorID = db.Column(db.Integer, db.ForeignKey('Proveedor.ID_Proveedor'), nullable=False)
 
     proveedor = db.relationship('Proveedor', back_populates='compras')
