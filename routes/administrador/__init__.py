e87ba266a759cb11c93412dcbe587e30d8689c37
natalia <nataliamelendez2426@gmail.com>
2025-10-21 15:09:07 -0500

    Auto-sync: file changes

--- routes/administrador/__init__.py
+++ routes/administrador/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
-# Importar el blueprint desde routes.py
+
 from .routes import admin
 
-# Hacemos visible el blueprint para que app.py lo pueda registrar
+
 __all__ = ["admin"]
